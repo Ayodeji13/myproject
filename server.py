141f725eebd2985ffd832e17ced11216a42e439c
Deji Onitilo <aonitilo@google.com>
2024-04-08 23:04:39 -0500

    first commit

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+print(__name__)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+@app.route('/')
+def hello_world():
+   return render_template('./index.html')
+
+@app.route('/blog')
+def blog():
+   return 'my thoughts on blogs'
